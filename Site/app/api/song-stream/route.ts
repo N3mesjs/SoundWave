52725dfa2c7386dfccaee33acdc767bdd09ffd25
N3mesjs <alessiodelsorbo390@gmail.com>
2025-12-19 02:51:01 +0100

    .

--- Site/app/api/song-stream/route.ts
+++ Site/app/api/song-stream/route.ts
@@ -1,31 +1,43 @@
 import { type NextRequest } from "next/server";
-
-import ytdl from "ytdl-core";
-import { Readable } from "node:stream";
+import { Innertube } from 'youtubei.js';
 
 export async function GET(request: NextRequest) {
   try {
     const searchParams = request.nextUrl.searchParams;
     const query = searchParams.get("query");
     const  range = request.headers.get("range");
+    //const url = `http://www.youtube.com/watch?v=${query}`;
+    const videoID = `aqz-KE-bpKQ`;
 
-    let info = await ytdl.getInfo("http://www.youtube.com/watch?v=aqz-KE-bpKQ");
+    const innertube = await Innertube.create(/* options */);
+    const info = await innertube.getBasicInfo(videoID);
+    const format = info.chooseFormat({
+      type: 'audio', 
+      quality: 'best',
+    })
+    const fileSize: number = format?.content_length;
 
-    const nodejsStream = ytdl("http://www.youtube.com/watch?v=aqz-KE-bpKQ", {
-      filter: "audioonly",
-      quality: "highestaudio",
+    const CHUNK_SIZE = 10 ** 6; // 1MB
+    const start = range ? Number(range?.replace(/\D/g, '')) : 0; //Replace non-digits to get the start byte with global flag
+    const end = Math.min(start + CHUNK_SIZE, fileSize - 1); //Calculate the end byte of the chunk, -1 because it starts from 0
+    const contentLength = end - start + 1; //Calculate content length of the chunk +1 because both start and end are inclusive, so from 0 to 9999 is 10000 bytes
+
+    const stream = await innertube.download(videoID, {
+      format: 'audio',
+      range: { start, end }
     });
-    const webStream = Readable.toWeb(nodejsStream) as ReadableStream;
-
-    return new Response(webStream, {
+    return new Response(stream, {
       headers: {
         "Content-Type": "audio/mpeg",
+        "Accept-Ranges": "bytes",
+        "Content-Range": `bytes ${start}-${end}/${fileSize}`, //Indicate the range of bytes being sent and the total file size
+        "Content-Length": contentLength.toString(),
       },
       status: 206,
     });
   } catch (error) {
     return Response.json(
-      { message: "Internal Server Error" },
+      { message: error.message || 'Internal Server Error' },
       {
         status: 500,
         headers: {
