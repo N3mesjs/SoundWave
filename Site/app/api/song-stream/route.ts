48063c633d16140d2125b2e27c928bd75eaf9b58
N3mesjs <alessiodelsorbo390@gmail.com>
2025-12-20 14:03:34 +0100

    adding implementation of a yt-dlp wrap

--- Site/app/api/song-stream/route.ts
+++ Site/app/api/song-stream/route.ts
@@ -1,4 +1,11 @@
 import { type NextRequest } from "next/server";
+import pgk from "yt-dlp-wrap";
+import { Readable } from "stream";
+import { join } from 'path';
+
+const YTDlpWrap = (pgk as any).default;
+const binaryPath = join('/', 'usr', 'bin', 'yt-dlp');
+const ytdlp = new YTDlpWrap(binaryPath);
 
 export async function GET(request: NextRequest) {
   try {
@@ -6,6 +13,44 @@ export async function GET(request: NextRequest) {
     const query = searchParams.get("query");
     const range = request.headers.get("range");
     const url = `http://www.youtube.com/watch?v=ZjBLbXUuyWg`;
+    
+    let readableStream = ytdlp.execStream([
+        url,
+        '-f', 
+        'bestaudio[ext=m4a]',
+        '--js-runtimes', 'node',
+    ]);
+
+    readableStream.on('error', (error) => {
+      console.error('Stream error:', error);
+      return Response.json(
+        { message: "Error streaming audio" },
+        {
+          status: 500,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+    });
+
+    readableStream.on('end', () => {
+      console.log('Stream ended');
+    });
+
+
+
+    // readableStream.pipe(createWriteStream('test.m4a'));
+
+    const stream = Readable.toWeb(readableStream);
+
+    return new Response(stream as ReadableStream, {
+      status: 200,
+      headers: {
+        "Content-Type": "audio/m4a",
+        "Transfer-Encoding": "chunked",
+      }
+    });
 
   } catch (error) {
     return Response.json(
--- Site/app/home/page.tsx
+++ Site/app/home/page.tsx
@@ -3,8 +3,7 @@ export default async function Page(){
     return(
         <>
             <h1>Home Page</h1>
-            <audio src ="/api/song-stream" controls />
-            <video src ="/api/song-stream" controls/>
+            <audio src="/api/song-stream" controls />
         </>
     )
 }
